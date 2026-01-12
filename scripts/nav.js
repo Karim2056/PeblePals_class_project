1f3968dbf80a25abc67307a854c979facadee0a6
Karim2056 <karimward8@gmail.com>
2026-01-12 11:58:18 +0000

    js progress

---
+++ scripts/nav.js
@@ -0,0 +1,31 @@
+function has_game_started() {
+    return false;
+}
+
+const nav_buttons = [
+    {"label": "HOME", "link": "/"},
+    {"label": "ABOUT", "link": "/about.html"},
+    {"label": "TESTIMONIALS", "link": "/testimonials.html"},
+    has_game_started()
+    ?{"label": "GAME", "link": "/game.html"} // ? means if
+    :{"label": "ADOPT", "link": "/adopt.html"}, // : means else
+];
+
+function is_this_active(page_name) {
+    let url = window.location.href;
+    let found = url.search(page_name.toLowerCase());
+    
+    if(found >= 0) {
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+let nav_container = document.getElementById("desktop-nav");
+
+for (let button of nav_buttons) {
+    nav_container.innerHTML += `<a class="nav-link
+        ${is_this_active(button.label) ? "active" : ""}
+    " href="${button.link}">${button.label}</a>`
+}
